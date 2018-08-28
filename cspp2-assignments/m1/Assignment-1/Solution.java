9ac30c0df4bfa50157c6fe07229965655e050454
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-28 14:06:40 +0530

    rool number name

---
+++ cspp2-assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,18 @@
+/**
+*@author vishnu
+*/
+public class Solution {
+    /**
+    *empty constructor
+    */
+    private Solution(){
+        //not using
+    }
+    /**
+    *main program 
+    * @param args is the parameter
+    */
+    public static void main(String[] args) {
+        System.out.println("Name : Teza Sri Vishnu R, Roll Number : 20186032 ");
+    }
+}
