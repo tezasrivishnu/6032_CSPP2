5fd0e01a68ec443a77d3f4f104d4117f9cfba85c
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-28 15:22:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,36 +1,51 @@
 import java.util.Scanner;
-import java.lang.Math;
-
+/**
+ * @author vishnu
+ */
 public final class Solution {
-	/**
-	*empty constructor.
-	*/
-	private Solution() {
-		//not using.
-	}
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		double d = (b * b) - (4 * a * c);
-		double r1 = (-b + Math.sqrt(d)) / (2 * a);
-		double r2 = (-b - Math.sqrt(d)) / (2 * a);
-		System.out.println(r1 + " " + r2);
-		// } else if (d == 0) {
-		// 	double r1 = (-b + Math.sqrt(d))/(2 * a);
-		// 	System.out.println(r1);
-		// } else {
-		// 	System.out.println("roots are imaginary");
-		// }
-	}
+    /**
+     * setting the value of 4.
+     */
+    private static final int four = 4;
+    /**
+    *empty constructor.
+    */
+    private Solution() {
+        //not using.
+    }
+    /*
+    Do not modify this main function.
+    */
+    /**
+     * main program
+     * @param args is the parameter
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+    /**
+     *calculating the roots
+     * @param a is int type
+     * @param b is int type
+     * @param c is int type
+     */
+    public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+        double d = (b * b) - (four * a * c);
+        double r1 = (-b + Math.sqrt(d)) / (2 * a);
+        double r2 = (-b - Math.sqrt(d)) / (2 * a);
+        System.out.println(r1 + " " + r2);
+        // } else if (d == 0) {
+        //  double r1 = (-b + Math.sqrt(d))/(2 * a);
+        //  System.out.println(r1);
+        // } else {
+        //  System.out.println("roots are imaginary");
+        // }
+    }
 }
