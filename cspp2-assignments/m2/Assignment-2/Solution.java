5dff109e68894614654932f5d8c0cac0e48cf39d
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-28 15:24:26 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -17,8 +17,8 @@ public final class Solution {
     Do not modify this main function.
     */
     /**
-     * main program
-     * @param args is the parameter
+     * main program.
+     * @param args is the parameter.
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -31,10 +31,10 @@ public final class Solution {
     Need to write the rootsOfQuadEq function and print the output.
     */
     /**
-     *calculating the roots
-     * @param a is int type
-     * @param b is int type
-     * @param c is int type
+     *calculating the roots.
+     * @param a is int type.
+     * @param b is int type.
+     * @param c is int type.
      */
     public static void rootsOfQuadEq(final int a, final int b, final int c) {
         double d = (b * b) - (FOUR * a * c);
