a138cd647d64af41a9547d809991bcdfc4fdc3b9
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-29 13:58:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -3,39 +3,41 @@ import java.util.Scanner;
  * Finding 7's below n.
  * @author tezasrivishnu.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution(){
-		//not using.
-	}
-	/**
-	 * Main program.
-	 * @param args is the parameter.
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int count = 0;
-		for (int i = 7; i <= n; i++) {
-			count += finding(i);
-		}
-	System.out.println(count);
-	}
-	/**
-	 * Calculating the 7's below i.
-	 * @param i int value is the parameter.
-	 * @return  an int value.
-	 */
-	public static int finding(int i) {
-		int count = 0;
-		while (i > 0) {
-			if (i % 10 == 7) {
-				count += 1;
-			}
-		i = i / 10;
-		}
-		return count;
-	}
+public final class Solution {
+    private static final int TEN = 10;
+    private static final int SEVEN = 7;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not using.
+    }
+    /**
+     * Main program.
+     * @param args is the parameter.
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int count = 0;
+        for (int i = SEVEN; i <= n; i++) {
+            count += finding(i);
+        }
+        System.out.println(count);
+    }
+    /**
+     * Calculating the 7's below i.
+     * @param i int value is the parameter.
+     * @return  an int value.
+     */
+    public static int finding(int i) {
+        int count = 0;
+        while (i > 0) {
+            if (i % TEN == SEVEN) {
+                count += 1;
+            }
+            i = i / TEN;
+        }
+        return count;
+    }
 }
