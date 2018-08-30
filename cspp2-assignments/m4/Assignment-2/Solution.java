e13c13c11e8151260ea2fa57dd721b8c23e94f47
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-30 14:27:30 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -23,8 +23,8 @@ class Solution {
 			for (int i = 0 ; i < m ; i++ )
 				for (int j = 0 ; j < y ; j++ )
 					sum[i][j] = a[i][j] + b[i][j];
-			for (int i = 0 ; i < m-1 ; i++ ){
-				for (int j = 0 ; j < n-1 ; j++ ){
+			for (int i = 0 ; i <= m-1 ; i++ ){
+				for (int j = 0 ; j <= n-1 ; j++ ){
 					System.out.print(sum[i][j] + " ");
 				}
 				System.out.println(sum[m-1][n-1]);
