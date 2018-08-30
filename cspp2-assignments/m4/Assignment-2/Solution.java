af256397ce251e7a76f3c07721629ce25639b8ec
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-30 14:37:51 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -2,37 +2,48 @@ import java.util.Scanner;
 import java.util.Arrays;
 /**
  * Adition of two matrices.
+ * @author tezasrivishnu.
  */
-class Solution {
-	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		int m = in.nextInt();
-		int n  = in.nextInt();
-		int a[][] = new int[m][n];
-		for (int i = 0 ; i < m ; i++ )
-			for (int j = 0 ; j < n ; j++ )
-				a[i][j] = in.nextInt();
-		int x = in.nextInt();
-		int y  = in.nextInt();
-		int b[][] = new int[x][y];
-		for (int i = 0 ; i < x ; i++ )
-			for (int j = 0 ; j < y ; j++ )
-				b[i][j] = in.nextInt();
-		if (m == x && n == y) {
-			int i;
-			int j;
-			int sum[][] = new int[m][y];
-			for (i = 0 ; i < m ; i++ )
-				for (j = 0 ; j < n ; j++ )
-					sum[i][j] = a[i][j] + b[i][j];
-			for (i = 0 ; i <= m-1 ; i++ ){
-				for (j = 0 ; j < n-1 ; j++ ){
-					System.out.print(sum[i][j] + " ");
-				}
-				System.out.println(sum[i][j]);
-			}
-		} else {
-			System.out.println("not possible");
-		}
-	}
+public final class Solution {
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //not using.
+    }
+    /**
+     * main program.
+     * @param args is the parameter.
+     */
+    public static void main(final String args[]) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n  = in.nextInt();
+        int a[][] = new int[m][n];
+        for (int i = 0 ; i < m ; i++ )
+            for (int j = 0 ; j < n ; j++ )
+                a[i][j] = in.nextInt();
+        int x = in.nextInt();
+        int y  = in.nextInt();
+        int b[][] = new int[x][y];
+        for (int i = 0 ; i < x ; i++ )
+            for (int j = 0 ; j < y ; j++ )
+                b[i][j] = in.nextInt();
+        if (m == x && n == y) {
+            int i;
+            int j;
+            int sum[][] = new int[m][y];
+            for (i = 0 ; i < m ; i++ )
+                for (j = 0 ; j < n ; j++ )
+                    sum[i][j] = a[i][j] + b[i][j];
+            for (i = 0 ; i <= m - 1 ; i++ ) {
+                for (j = 0 ; j < n - 1 ; j++ ) {
+                    System.out.print(sum[i][j] + " ");
+                }
+                System.out.println(sum[i][j]);
+            }
+        } else {
+            System.out.println("not possible");
+        }
+    }
 }
