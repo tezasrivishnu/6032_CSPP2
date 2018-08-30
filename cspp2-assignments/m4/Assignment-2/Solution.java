c6d0f7d7b4477047ff7bf6d3508c995a32b5ebc4
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-30 14:47:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -27,8 +27,8 @@ public final class Solution {
         int x = in.nextInt();
         int y  = in.nextInt();
         int b[][] = new int[x][y];
-        for (int i = 0; i < x ;i++) {
-            for (int j = 0 ; j < y; j++) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
                 b[i][j] = in.nextInt();
             }
         }
@@ -37,7 +37,7 @@ public final class Solution {
             int j;
             int sum[][] = new int[m][y];
             for (i = 0; i < m; i++) {
-                for (j = 0 ; j < n ; j++) {
+                for (j = 0; j < n; j++) {
                     sum[i][j] = a[i][j] + b[i][j];
                 }
             }
