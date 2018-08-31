f496729f22345deb8574d6b191f00787c7c2a5b0
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-31 12:00:51 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -4,47 +4,38 @@ import java.util.Scanner;
  *program to convert string binary value to decimal.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//not using
-	}
-	/**
-	 * main program.
-	 * @param args is the parameter.
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		reading(sc, n);
-	}
-	public static void reading(Scanner sc,final int n){
-		for (int i = 0; i < n; i++) {
-			String s = sc.next();
-			binaryToDecimal(s);
-			// System.out.println((int)res);
-		}
-	}
-	/**
-	 * converting binary to decimal.
-	 * @param s string is the parameter.
-	 * @return  double value.
-	 */
-	public static void binaryToDecimal(final String s) {
-		int a = s.length();
-		double sum = 0;
-		double temp = 0;
-		long b = a;
-		for (int i = 0; i <= a - 1; i++) {
-			long n = Character.getNumericValue(s.charAt(i));
-			sum = (n * Math.pow(2, b - 1));
-			temp += sum;
-			b -= 1;
-		}
-		result(temp);
-	}
-	static void result(final double sum) {
-		System.out.println((int) sum);
-	}
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //not using
+    }
+    /**
+     * main program.
+     * @param args is the parameter.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            String s = sc.next();
+            double res = binaryToDecimal(s);
+            System.out.println((int)res);
+        }
+    }
+    /**
+     * converting binary to decimal.
+     * @param s string is the parameter.
+     * @return  double value.
+     */
+    static double binaryToDecimal(final String s) {
+        int a = s.length();
+        double sum = 0;
+        long b = a;
+        for (int i = 0; i <= a - 1; i++) {
+            long n = Character.getNumericValue(s.charAt(i));
+            sum = sum + (n * Math.pow(2, b - 1));
+            b -= 1;
+        } return sum;
+    }
 }
