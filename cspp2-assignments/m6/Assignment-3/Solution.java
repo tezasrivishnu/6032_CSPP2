1723abfe98563e30c3f17741a49ea64c916dc545
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-01 10:46:35 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-3/Solution.java
+++ cspp2-assignments/m6/Assignment-3/Solution.java
@@ -6,55 +6,55 @@ import java.util.Arrays;
  *  character with the given replacement.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//not used
-	}
-	/**
-	 * Function to replace each element
-	 *  of the given char array that matches the given
-	 *  character with the given replacement.
-	 *
-	 * @param      charArray  The character array
-	 * @param      oldChar    The old character
-	 * @param      newChar    The new character
-	 *
-	 * @return     new character array with replaced characters
-	 */
-	static String replaceAll(final char[] charArray,
-	                         final char oldChar, final char newChar) {
-		String str = new String(charArray);
-		String temp = "";
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == oldChar) {
-				temp += newChar;
-			} else {
-				temp += str.charAt(i);
-			}
-		}
-		return temp;
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		char[] charArray = new char[n];
-		for (int i = 0; i < n; i++) {
-			charArray[i] = scan.next().charAt(0);
-		}
-		char oldChar = scan.next().charAt(0);
-		char newChar = scan.next().charAt(0);
-		String newCharArray = replaceAll(charArray, oldChar, newChar);
-		char[] ch = new char[newCharArray.length()];
-		for (int i =0;i<newCharArray.length(); i++ ) {
-			ch[i] = newCharArray.charAt(i);
-		}
-		System.out.println(Arrays.toString(ch));
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to replace each element
+     *  of the given char array that matches the given
+     *  character with the given replacement.
+     *
+     * @param      charArray  The character array
+     * @param      oldChar    The old character
+     * @param      newChar    The new character
+     *
+     * @return     new character array with replaced characters
+     */
+    static String replaceAll(final char[] charArray,
+                             final char oldChar, final char newChar) {
+        String str = new String(charArray);
+        String temp = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == oldChar) {
+                temp += newChar;
+            } else {
+                temp += str.charAt(i);
+            }
+        }
+        return temp;
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        char[] charArray = new char[n];
+        for (int i = 0; i < n; i++) {
+            charArray[i] = scan.next().charAt(0);
+        }
+        char oldChar = scan.next().charAt(0);
+        char newChar = scan.next().charAt(0);
+        String newCharArray = replaceAll(charArray, oldChar, newChar);
+        char[] ch = new char[newCharArray.length()];
+        for (int i = 0; i < newCharArray.length(); i++ ) {
+            ch[i] = newCharArray.charAt(i);
+        }
+        System.out.println(Arrays.toString(ch));
+    }
 }
