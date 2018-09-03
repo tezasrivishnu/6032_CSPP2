96e0584d393f9e4ae8b7dc95c24ba7865d1d4f18
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-03 19:00:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -16,7 +16,7 @@ class InputValidator {
      * setting the input.
      * @param input string is the parameter.
      */
-    public InputValidator(final String input) {
+    public InputValidator(String input) {
         this.input = input;
     }
     /**
