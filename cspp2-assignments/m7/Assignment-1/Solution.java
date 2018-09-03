2d650ada96ade5d6ab464e3230fef40ca06417c2
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-03 18:21:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -4,35 +4,39 @@ import java.util.Scanner;
  * @author tezasrivishnu.
  */
 class InputValidator {
-	private String input;
-	/**
-	 * setting the input.
-	 * @param input string is the parameter.
-	 */
-	public InputValidator(String input) {
-		this.input = input;
-	}
-	/**
-	 * validating the data.
-	 * @param input string is the parameter.
-	 * @return boolean value.
-	 */
-	public static boolean validateData(String input) {
-		if (input.length() >= 6) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /**
+     * setting value of 6.
+     */
+    private static final int SIX = 6;
+    String input;
+    /**
+     * setting the input.
+     * @param input string is the parameter.
+     */
+    public InputValidator(final String input) {
+        this.input = input;
+    }
+    /**
+     * validating the data.
+     * @param input string is the parameter.
+     * @return boolean value.
+     */
+    public static boolean validateData(final String input) {
+        if (input.length() >= SIX) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 /**
  * Class for printing the answer.
  */
 public class Solution {
-	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData(input));
-	}
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData(input));
+    }
 }
