85fef2152b98a96c21be285926a36cf943cb78fe
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-06 09:52:34 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Solution.java
@@ -1,13 +1,25 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
-
-public class Solution {
-	
-
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ * @author tezasrivishnu.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not using.
+    }
+    /**
+     * main program fot the list string.
+     * @param      args  is the input parameter.
+     */
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l=(StringListInterface)sl;//Typecasting is done for StringListInterface
+        StringListInterface l = (StringListInterface)sl;
+        //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -18,41 +30,41 @@ public class Solution {
             String[] tokens = line.split(" ");
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
-                case "add":
-	                l.add(tokens[1]);
+            case "add":
+                l.add(tokens[1]);
                 break;
-                case "addAll":
-                if(tokens.length==2){
-                String[] t1 = tokens[1].split(",");
-                l.addAll(t1);
+            case "addAll":
+                if (tokens.length == 2) {
+                    String[] t1 = tokens[1].split(",");
+                    l.addAll(t1);
                 }
                 break;
-                case "size":
+            case "size":
                 // invoke size method and print the list size
                 // BTW, list size is not the array size
                 // it is the number of items in the list
                 System.out.println(l.size());
                 break;
-                case "print":
+            case "print":
                 // print the list (implement toString in StringList class
                 //for this to work)
                 // expected format is [item-1,item-2,...,item-n]
                 // review the output testcase file
                 System.out.println(l);
                 break;
-                case "remove":
+            case "remove":
                 l.remove(Integer.parseInt(tokens[1]));
                 break;
-                case "indexOf":
+            case "indexOf":
                 System.out.println(l.indexOf(tokens[1]));
                 break;
-                case "get":
+            case "get":
                 System.out.println(l.get(Integer.parseInt(tokens[1])));
                 break;
-                case "contains":
+            case "contains":
                 System.out.println(l.contains(tokens[1]));
                 break;
             }
         }
-	}
+    }
 }
