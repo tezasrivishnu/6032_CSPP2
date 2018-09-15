efb38c58ee240144ffb4a7599200632e2b569ea5
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-15 12:10:15 +0530

    "check_style_score -> 5 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -101,7 +101,7 @@ public final class Solution {
                 String[] arrays = string.split(" ");
                 array[i] = Integer.parseInt(arrays[1]);
                 System.out.println(quizarr[i].getQuestion() + "(" + quizarr[i].getMarks() + ")");
-                System.out.println(choices[0] + "        " + choices[1] + "        " + choices[2] + "        " + choices[3]);
+                System.out.println(choices[0] + "\t" + choices[1] + "\t" + choices[2] + "\t" + choices[3]);
                 System.out.println();
             }
         }
