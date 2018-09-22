38be065bd1420cd78546e197eec3c758bda91ac4
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-22 12:40:30 +0530

    "check_style_score -> 14 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -117,15 +117,46 @@ public class TodoistMain {
 		startTest();
 	}
 }
+/**
+ * Class for task.
+ */
 class Task {
+	/**
+	 * declaring string title.
+	 */
 	private String title;
+	/**
+	 * declaring string assignnedTo.
+	 */
 	private String assignedTo;
+	/**
+	 * declaring int timetocomplete.
+	 */
 	private int timeToComplete;
+	/**
+	 * declaring string important or not.
+	 */
 	private String important;
+	/**
+	 * declaring string urgent.
+	 */
 	private String urgent;
+	/**
+	 * declaring string status.
+	 */
 	private String status;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      tasktitle       The tasktitle
+	 * @param      taskassignedto  The taskassignedto
+	 * @param      tasktime        The tasktime
+	 * @param      imp             The imp
+	 * @param      urg             The urg
+	 * @param      status          The status
+	 */
 	Task(final String tasktitle, final String taskassignedto,
-		final int tasktime, final boolean imp, final boolean urg, final String status) {
+	     final int tasktime, final boolean imp, final boolean urg, final String status) {
 		this.title = tasktitle;
 		this.assignedTo = taskassignedto;
 		this.timeToComplete = tasktime;
@@ -141,24 +172,59 @@ class Task {
 		}
 		this.status = status;
 	}
+	/**
+	 * Gets the title.
+	 *
+	 * @return     The title.
+	 */
 	public String getTitle() {
 		return this.title;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String getassignedTo() {
 		return this.assignedTo;
 	}
+	/**
+	 * Gets the time.
+	 *
+	 * @return     The time.
+	 */
 	public int getTime() {
 		return this.timeToComplete;
 	}
+	/**
+	 * Gets the important.
+	 *
+	 * @return     The important.
+	 */
 	public String getImportant() {
 		return this.important;
 	}
+	/**
+	 * Gets the urgent.
+	 *
+	 * @return     The urgent.
+	 */
 	public String getUrgent() {
 		return this.urgent;
 	}
+	/**
+	 * Gets the status.
+	 *
+	 * @return     The status.
+	 */
 	public String getStatus() {
 		return this.status;
 	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		String s = "";
 		s += this.title + ", ";
@@ -169,6 +235,11 @@ class Task {
 		s += this.status;
 		return s;
 	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public void totoString() {
 		String s = "";
 		s += this.title + ", ";
@@ -180,14 +251,33 @@ class Task {
 		System.out.println(s);
 	}
 }
+/**
+ * Class for todoist.
+ */
 class Todoist {
+	/**
+	 * declaring list array for TASK class.
+	 */
 	List<Task> taskarr;
+	/**
+	 * Constructs the object.
+	 */
 	Todoist() {
 		taskarr = new List<Task>();
 	}
+	/**
+	 * Adds a task.
+	 *
+	 * @param      task  The task
+	 */
 	public void addTask(final Task task) {
 		taskarr.add(task);
 	}
+	/**
+	 * calculating the total time.
+	 *
+	 * @return     total time.
+	 */
 	public int totalTime4Completion() {
 		int time = 0;
 		for (int i = 0; i < taskarr.size(); i++) {
@@ -197,34 +287,51 @@ class Todoist {
 		}
 		return time;
 	}
+	/**
+	 * Gets the next task.
+	 *
+	 * @param      name  The name
+	 *
+	 * @return     The next task.
+	 */
 	public Task getNextTask(final String name) {
 		for (int i = 0; i < taskarr.size(); i++) {
 			if (name.equals(taskarr.get(i).getassignedTo()) &&
-				(((taskarr.get(i).getImportant()).equals("Important"))
-				 && ((taskarr.get(i).getUrgent()).equals("Not Urgent"))
-				 && ((taskarr.get(i).getStatus()).equals("todo")))) {
+			        (((taskarr.get(i).getImportant()).equals("Important"))
+			         && ((taskarr.get(i).getUrgent()).equals("Not Urgent"))
+			         && ((taskarr.get(i).getStatus()).equals("todo")))) {
 				return taskarr.get(i);
 			}
 		}
 		return null;
 	}
+	/**
+	 * Gets the next task for a person.
+	 *
+	 * @param      name  The name
+	 * @param      num   The number
+	 *
+	 * @return     The next task.
+	 */
 	public Task[] getNextTask(final String name, final int num) {
 		Task[] ne = new Task[num];
 		int count = 0;
 		for (int i = 0; i < taskarr.size(); i++) {
 			if (name.equals(taskarr.get(i).getassignedTo())
-				&& (taskarr.get(i).getStatus()).equals("todo")) {
+			        && (taskarr.get(i).getStatus()).equals("todo")) {
 				ne[count] = taskarr.get(i);
 				count += 1;
-			} if(count == num) {
+			} if (count == num) {
 				return ne;
 			}
 		}
 		return ne;
 	}
-
-
-
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public void totoString() {
 		for (int i = 0; i < taskarr.size(); i++) {
 			taskarr.get(i).totoString();
